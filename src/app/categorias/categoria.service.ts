ade351026908582327b835ff199d3acba566774c
Aluno <aluno@eteclencois.com.br>
2019-04-15 11:44:48 -0300

    criação de paginação

--- src/app/categorias/categoria.service.ts
+++ src/app/categorias/categoria.service.ts
@@ -1,6 +1,11 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+export class CategoriaFiltro{
+  nome: string;
+  pagina = 0;
+  itensporpagina = 2;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +15,33 @@ export class CategoriaService {
   constructor(private http: HttpClient) { }
 
   categoriaUrl = 'http://localhost:8080/categorias';
+
+  pesquisar(filtro: CategoriaFiltro): Promise<any>{
+    let params = new HttpParams({
+      fromObject : {
+        page: filtro.pagina.toString(),
+        size: filtro.itensporpagina.toString()
+      }
+    });
+
+    if(filtro.nome){
+      params = params.append('nome', filtro.nome);
+    }
+    
+    return this.http.get<any>(`${this.categoriasUrl}`, {params})
+    .toPromise()
+    .then(response => {
+      const categorias = response.content;
+
+      const resultado = {
+        categorias,
+        total: response.totalElements;
+      }
+
+      return resultado;
+    
+    });
+
+
+  }
 }
